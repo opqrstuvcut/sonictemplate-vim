be2adea174fd169014dc189d033179a078f7241c
opqrstuvcut <vikz2713@gmail.com>
2019-03-02 18:31:13 +0900

    argparse追加

--- template/python/base-main.py
+++ template/python/base-main.py
@@ -1,8 +1,16 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
+import argparse
+
+def parse_argument():
+    parser = argparse.ArgumentParser("", add_help=True)
+    parser.add_argument()
+    args = parser.parse_args()
+    return args
 
 
 def main():
+	args= parse_argument()
 	{{_cursor_}}
 
 if __name__ == '__main__':
